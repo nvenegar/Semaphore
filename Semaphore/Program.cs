8d55468097062f3854938c2959b7e57077cc5655
Nelson Venegas <nvenegar@hotmail.com>
2016-11-04 22:21:21 -0500

    Agregar archivos de proyecto.

---
+++ Semaphore/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+namespace SemaphoreThreading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        for (int i = 0; i < 10; i++)
+            {
+                threads[i] = new Thread(C_sharpcorner);
+                threads[i].Name = "thread_" + i;
+                threads[i].Start();
+            }
+           Console.Read();
+
+        }
+
+       static Thread[] threads = new Thread[10];
+        static Semaphore sem = new Semaphore(3, 3);
+        static void C_sharpcorner()
+        {
+            Console.WriteLine("{0} en Fila", Thread.CurrentThread.Name);
+            sem.WaitOne();
+            Console.WriteLine("{0} en atencion", Thread.CurrentThread.Name);
+            Thread.Sleep(300);
+            Console.WriteLine("{0} Saliendo", Thread.CurrentThread.Name);
+            sem.Release();
+        }
+ 
+    }
+}
+
+
